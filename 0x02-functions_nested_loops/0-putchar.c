14ea636d1dd3f6b92171c26b2fc84c1caa3abc8a
Chandrasura25 <abdulganiy1624demilade@gmail.com>
2022-04-12 00:46:56 -0700

    0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+char string[8] = "_putchar";
+int n = 0;
+while (n < 8)
+{
+_putchar(string[n]);
+++n;
+}
+_putchar(10);
+return (0);
+}
