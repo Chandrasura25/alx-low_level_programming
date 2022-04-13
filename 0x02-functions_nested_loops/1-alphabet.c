02aeccebb3bbd8659223a4c90600494b3fcb01ec
Chandrasura25 <abdulganiy1624demilade@gmail.com>
2022-04-13 02:13:59 -0700

    1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+void print_alphabet(void)
+{
+char a = 'a';
+while (a <= 'z')
+{
+_putchar(a);
+++a;
+}
+_putchar(10);
+}
