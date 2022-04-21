9a4bdbe1098b07ae05ed7c0aee186d32f419286e
Chandrasura25 <abdulganiy1624demilade@gmail.com>
2022-04-21 09:14:08 -0700

    8

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -8,7 +8,7 @@
  *Numbers should be displayed in the same order they are stored in the array
  *You can only use _putchar to print
  */
-void print_array(int *a, int *b)
+void print_array(int *a, int *n)
 {
 int i;
 i = 0;
