e01cf170c2741ba6b67b7b0f700a627f575b04cd
Chandrasura25 <abdulganiy1624demilade@gmail.com>
2022-04-24 21:27:21 -0700

    trying to fix

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -3,19 +3,20 @@
  * _strcat - concatinate 2 strings
  * @dest: first string
  * @src: second string
- * Return: string.
+ * Return: the pointer to dest.
  */
 char *_strcat(char *dest, char *src)
 {
 int i = 0, j = 0;
-while (dest[i] != '\0')
+while ((dest + i) != '\0')
 i++;
-while (src[j] != '\0')
+while (j >= 0)
 {
 dest[i] = src[j];
+if (*(src + j) == '\0')
+break;
 i++;
 j++;
 }
-dest[i] = '\0';
 return (dest);
 }
